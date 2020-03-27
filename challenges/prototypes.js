46493e517b30711d8ccb5cabebb37832e45b6ef6
unknown <tonybrat74@gmail.com>
2020-03-27 10:59:52 -0700

    1

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,9 +5,7 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */Function CubiodMaker( length, width, height){
-this.length = length;
-this.width = width;
-this.height= height;
+
 } 
 
 
